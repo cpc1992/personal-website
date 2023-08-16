5615f82a0756a0e65785ee3f669b2527832a51f6
Ninja Cliff <cliffordpchan@gmail.com>
2023-08-16 00:30:02 -0700

    a ton of styling on the main page

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,15 +1,15 @@
 import { useState } from "react";
 import "./App.css";
 import Header from "./Header.jsx";
+import Footer from "./Footer.jsx";
 import ContextBox from "./ContentBox.jsx";
 
 export default function App() {
   return (
-    <div className="margin-box">
-      <div className="shadowed-box">
-        <Header />
-        <ContextBox />
-      </div>
+    <div className="shadowed-box">
+      <Header />
+      <ContextBox />
+      <Footer />
     </div>
   );
 }
--- frontend/src/Details.jsx
+++ frontend/src/Details.jsx
@@ -1,4 +1,4 @@
-import { useState } from "react";
+import "./Details.css";
 
 export default function Details({ projects, toggler, toggle }) {
   let noneSelected = true;
@@ -13,55 +13,58 @@ export default function Details({ projects, toggler, toggle }) {
     <div className="details-box">
       <div className="details-holder">
         <div className="hidden">
-          <h2 className="welcome">Hello There</h2>
-          <span>
-            Hi my name is Clifford Chan. I'm a software developer living in the
-            Bay Area. Lorem ipsum dolor sit amet, consectetur adipiscing elit,
-            sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.
-            Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris
-            nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in
-            reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla
-            pariatur. Excepteur sint occaecat cupidatat non proident, sunt in
-            culpa qui officia deserunt mollit anim id est laborum.
-          </span>
+          <h1 className="welcome">Hello There</h1>
+          <p className="para-text">
+            Hi my name is Clifford Chan. I'm a full stack software developer
+            living in the Bay Area. I am passionate about solving problems. I am
+            the type of person who will spend hours writing a script to save me
+            5 minutes a day. I love learning new technologies and expanding my
+            skillset.
+            <br />
+            <br />
+            When I'm not coding you can find me...
+          </p>
         </div>
 
         <div
-          className={"text1 " + (noneSelected ? "left_visible" : "left_gone")}
+          className={
+            "absolute-positioning " +
+            (noneSelected ? "open-left-trans" : "closed-left-trans")
+          }
         >
           <h2 className="welcome">Welcome</h2>
-          <span>
-            Hi my name is Clifford Chan. I'm a software developer living in the
-            Bay Area. Lorem ipsum dolor sit amet, consectetur adipiscing elit,
-            sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.
-            Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris
-            nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in
-            reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla
-            pariatur. Excepteur sint occaecat cupidatat non proident, sunt in
-            culpa qui officia deserunt mollit anim id est laborum.
-          </span>
+          <p className="para-text">
+            Hi my name is Clifford Chan. I'm a full stack software developer
+            living in the Bay Area. I am passionate about solving problems. I am
+            the type of person who will spend hours writing a script to save me
+            5 minutes a day. I love learning new technologies and expanding my
+            skillset.
+            <br />
+            <br />
+            When I'm not coding you can find me...
+          </p>
         </div>
         {projects.map((project) => {
           return (
             <div
               key={project.id}
               className={
-                "text1 " +
+                "absolute-positioning " +
                 (project.state == 0
-                  ? "test0"
+                  ? "closed-right"
                   : project.state == 1
-                  ? "test1"
+                  ? "closed-right-trans"
                   : project.state == 2
-                  ? "test2"
+                  ? "open-right"
                   : project.state == 3
-                  ? "test3"
+                  ? "open-right-trans"
                   : project.state == 4
-                  ? "test4"
-                  : "test5")
+                  ? "open-left"
+                  : "closed-left-trans")
               }
             >
               <h2 className="project-title">{project.title}</h2>
-              <span>{project.description}</span>
+              <p className="para-text">{project.description}</p>
             </div>
           );
         })}
